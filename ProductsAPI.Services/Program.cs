c82b8204d11825b286262907a898d29bcf7b4736
andygap <anderson.gap@hotmail.com>
2023-07-26 19:48:57 -0300

    first commit

---
+++ ProductsAPI.Services/Program.cs
@@ -0,0 +1,26 @@
+using ProductsAPI.Infra.IoC.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServerConfig(builder.Configuration); //SqlServer
+builder.Services.AddMongoDBConfig(builder.Configuration); //MongoDB
+builder.Services.AddDependencyInjection(); //Serviços
+builder.Services.AddMediatRConfig(); //MediatR
+builder.Services.AddJwtBearerConfig(builder.Configuration); // JWT
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { };
